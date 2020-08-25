a07a5c379fdfaa31ac64e3347b7c69c17656e967
asadalicode <69710973+asadalicode@users.noreply.github.com>
2020-08-25 12:06:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const jsonServer = require('json-server');
-const auth = require('json-server-auth');
-const app = jsonServer.create();
-const router = jsonServer.router('db.json');
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-// /!\ Bind the router db to the app
-app.db = router.db;
+server.use(middlewares);
 
-// You must apply the auth middleware before the router
-app.use(auth);
-app.use(router);
-app.listen(3000);
+server.use(router);
+server.listen(3000);
