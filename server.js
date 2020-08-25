ba797a198f339ad22e2db4b5a3e7cb147dd6d396
asadalicode <69710973+asadalicode@users.noreply.github.com>
2020-08-25 14:33:51 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const jsonServer = require('json-server');
-const auth = require('json-server-auth');
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-// const middlewares = jsonServer.defaults();
+// const router = jsonServer.router("db.json");
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.db = router.db;
 
-// server.use(middlewares);
+server.use(middlewares);
 server.use(auth);
 server.use(router);
 
