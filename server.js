c0873dd343d6e34e3fc9eacc39cdacdbf64ec413
asadalicode <69710973+asadalicode@users.noreply.github.com>
2020-09-11 22:34:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const server = jsonServer.create();
+const server_1 = jsonServer.create();
 // const router = jsonServer.router("db.json");
 const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-server.use('/static', express.static(path.join(__dirname, 'public')))
+server_1.use('/static', express.static(path.join(__dirname, 'public')))
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
@@ -16,3 +17,14 @@ server.use(auth);
 server.use(router);
 
 server.listen(port);
+
+// Avoid CORS issue
+server_1.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+server_1.use(router)
+
+server_1.listen(3004)
